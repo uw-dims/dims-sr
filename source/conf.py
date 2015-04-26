3485c475564ccc2e786855691b13dc8afa8cee4c
Dave Dittrich <dave.dittrich@gmail.com>
2015-04-25 22:31:42 -0700

    Fix intersphinx links to use HOME environment variable

--- source/conf.py
+++ source/conf.py
@@ -346,10 +346,10 @@ epub_exclude_files = ['search.html']
 
 intersphinx_cache_limit = -1   # days to keep the cached inventories (0 == forever)
 intersphinx_mapping = {
-        'dimsocd': ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-ocd',
-                    ('../../dims-ocd/build/html/objects.inv',None)),
-        'dimsad': ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-ad',
-                    ('../../dims-ad/build/html/objects.inv',None)),
-        'dimstp': ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-tp',
-                    ('../../dims-tp/build/html/objects.inv',None))
+        'dimsocd': ("%s/dims/docs/dims-ocd" % os.environ['HOME'],
+                    ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-ocd/objects.inv', None)),
+        'dimsad': ("%s/dims/docs/dims-ad" % os.environ['HOME'],
+                    ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-ad/objects.inv', None)),
+        'dimstp': ("%s/dims/docs/dims-tp" % os.environ['HOME'],
+                    ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-tp/objects.inv', None))
 }
